5c2b200d97da6683fefd1cac4e306383055e2bec
Austin Johnson <technicallyaustin@gmail.com>
2021-03-05 17:35:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-    if time.to_i > 12
+    if time.to_i < 12
       resp.write "Good Afternoon!"
-      elsif time.to_i < 12
+      elsif time.to_i > 12
       resp.write "Good Morning!"
     end
     resp.finish
